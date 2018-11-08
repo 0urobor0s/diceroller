fd37962f5d1cf90607eede0acadb277216a41c1e
0urobor0s <lhome.gamer@gmail.com>
2018-11-08 15:27:16 +0000

    Generator

--- lib/mix/tasks/gen.input.ex
+++ lib/mix/tasks/gen.input.ex
@@ -2,15 +2,10 @@ defmodule Mix.Tasks.Gen.Input do
   use Mix.Task
 
   def run(args) do
-    #File.read!("input.txt") 
-    #|> String.split("\n") 
-    #|> List.delete("")
-    #|> DiceRoler.sroler
     cond do
       length(args) == 0 ->
         Mix.shell.info "Needs to receive a number."
       true ->
-        #Benchmark.measure(fn -> create(args) end)
         create(args)
     end
   end
